a3d0da7e8c1dc701ee9ce12cfc1a866951d2542e
Edmond Wu <edmond.wu@velocitylight.net>
2021-02-26 16:38:08 +0800

    completed UX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import imageCompression from "browser-image-compression";
 import ImageItem from "./components/ImageItem";
 import "./App.scss";
-
 
 const options = {
   maxSizeMB: 1,
@@ -10,45 +10,63 @@ const options = {
   useWebWorker: true,
 };
 
+const getImagesFormat = (files) => {
+  let result = [];
+  for (let file of files) {
+    result.push({
+      id: uuidv4(),
+      filename: file.name,
+      loadedCompleted: false,
+      imageSrc: "",
+      file,
+    });
+  }
+  return result;
+};
 
-const getDataUrl = (file, setImages) => {
+const getDataUrl = (file, id, images, setImages) => {
   const reader = new FileReader();
 
   reader.onload = (e) => {
-    const imagePayload = {
-      imageSrc: e.target.result,
-    };
-    setImages((state) => ([ ...state, imagePayload ]));
+    const currentIndex = images.findIndex((image) => image.id === id);
+    const currentImage = images[currentIndex];
+    currentImage.imageSrc = e.target.result;
+    currentImage.loadedCompleted = true;
+    setImages(images);
   };
 
   reader.readAsDataURL(file);
 };
 
-const getTransferData = (setImages) => async (event) => {
+const getTransferData = (images, setImages) => async (event) => {
   event.preventDefault();
   event.stopPropagation();
-  await compressImage(event.dataTransfer.files[0], setImages);
+
+  for await (let file of event.dataTransfer.files) {
+    await compressImage(file, images, setImages);
+  }
 };
 
+const getUploadImage = (images, setImages) => async (event) => {
+  const files = event.target.files;
 
-const handleImageUpload = (setImages) => async (event) => {
-  await compressImage(event.target.files[0], setImages); 
-}
+  const imageSet = getImagesFormat(files);
+  setImages((state) => [...state, ...imageSet]);
 
-const compressImage = async (imageFile, setImages) => {
+  for await (let image of imageSet) {
+    await compressImage(image.file, image.id, [...images, ...imageSet], setImages);
+  }
+};
+
+const compressImage = async (imageFile, id, images, setImages) => {
   console.log("是否為 Blob", imageFile instanceof Blob); // true
   console.log(`壓縮前檔案大小 ${imageFile.size / 1024 / 1024} MB`);
 
   try {
     const compressedFile = await imageCompression(imageFile, options);
-    console.log(
-      "compressedFile instanceof Blob",
-      compressedFile instanceof Blob
-    ); // true
     console.log(`壓縮後檔案大小 ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
     console.log(`少了 ${100 - (compressedFile.size / imageFile.size) * 100}%`); // smaller than maxSizeMB
-    getDataUrl(compressedFile, setImages);
-    // await uploadToServer(compressedFile); // write your own logic
+    getDataUrl(compressedFile, id, images, setImages);
   } catch (error) {
     console.log(error);
   }
@@ -63,34 +81,31 @@ function App() {
   const [images, setImages] = useState([]);
   const dropAreaRef = useRef(null);
 
-  useEffect(() => {
-    let register = false;
+  const handleTransferData = getTransferData(images, setImages);
+  const handleImageUpload = getUploadImage(images, setImages);
 
-    if (dropAreaRef !== null && !register) {
-      register = true;   
-      dropAreaRef.current.addEventListener("dragover", preventDefault, false);
-      dropAreaRef.current.addEventListener(
-        "drop",
-        getTransferData(setImages),
-        false
-      );
+  useEffect(() => {
+    const dropArea = dropAreaRef.current;
+
+    if (dropArea !== null) {
+      dropArea.addEventListener("dragover", preventDefault, false);
+      dropArea.addEventListener("drop", handleTransferData, false);
     }
 
     return () => {
-      console.log('undemouny')
-      if (dropAreaRef !== null) {
-        dropAreaRef.current.removeEventListener("dragover", preventDefault);
-        dropAreaRef.current.removeEventListener("drop", getTransferData(setImages));
+      if (dropArea !== null) {
+        dropArea.removeEventListener("dragover", preventDefault);
+        dropArea.removeEventListener("drop", handleTransferData);
       }
     };
-  }, [dropAreaRef]);
+  }, [dropAreaRef, handleTransferData]);
 
   return (
     <div className='App'>
       <div ref={dropAreaRef} className='image-container'>
         <div>
-          {images.map((item) => {
-            return <ImageItem key={item.filename} {...item} />;
+          {images.map((item, index) => {
+            return <ImageItem key={`${item.filename}_${index}`} {...item} />;
           })}
         </div>
         <label htmlFor='upload_file' className='upload-image-button'>
@@ -98,10 +113,11 @@ function App() {
         </label>
       </div>
       <input
+        multiple
         id='upload_file'
         type='file'
         className='file_input'
-        onChange={handleImageUpload(setImages)}
+        onChange={handleImageUpload}
       />
     </div>
   );
--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -3,10 +3,12 @@ import React from "react";
 const ImageItem = ({ filename, loadedCompleted, imageSrc }) => {
   return (
     <div className='image-item'>
+      { !loadedCompleted && <div className="loading-text">壓縮中</div> }
       <div
         className='image'
         style={{ backgroundImage: `url(${imageSrc})` }}
       ></div>
+      <div className="filename">{ filename }</div>
     </div>
   );
 };
